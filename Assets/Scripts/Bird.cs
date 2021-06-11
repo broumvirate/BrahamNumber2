77a3a4cf27a945694aaad528742b54532de40e9b
HenBagle <ben.hagle@gmail.com>
2021-06-11 19:46:00 -0400

    hey, I'm actually unity

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    #region Variables
+
+    private Rigidbody2D rb;
+
+    public float speed;
+    public float max_speed;
+    public float jumpVelocity;
+
+    protected bool isOnGround
+    {
+        get;
+        private set;
+    }
+    
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("HorizontalBird");
+        float moveBy = x * speed;
+        if (moveBy != 0f)
+        {
+            rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x * 0.7f, rb.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+    }
+}
