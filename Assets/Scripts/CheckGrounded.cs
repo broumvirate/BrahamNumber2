478a036dc45b527c6d77cc1961644eb0a06393e9
AldenBurgess <60455940+AldenBurgess@users.noreply.github.com>
2021-06-11 23:22:41 -0400

    runner located
    Added dexter locomotion, improved CheckGrounded

--- Assets/Scripts/CheckGrounded.cs
+++ Assets/Scripts/CheckGrounded.cs
@@ -15,11 +15,14 @@ public class CheckGrounded : MonoBehaviour
     //check if the object is on the ground by detecting a raycast hit below it
     private void Update()
     {
-        RaycastHit2D groundCheck = Physics2D.Raycast(transform.position, Vector2.down, distToGround + 0.1f, groundLayer);
-        
+        //dont look at this part or i'll kill you
+        //RaycastHit2D groundCheck = Physics2D.CapsuleCast(GetComponent<BoxCollider2D>().bounds.center, new Vector2(GetComponent<BoxCollider2D>().bounds.center.x, GetComponent<BoxCollider2D>().bounds.min.y - 0.1f) , CapsuleDirection2D.Vertical, 180.0f, Vector2.down, 0.1f, groundLayer);
+        //rather than use a capsule collider (i dunno how to lol) i'm using two raycasts, sourced from either side of the sprite.
+        RaycastHit2D groundCheck = Physics2D.Raycast(new Vector2(transform.position.x - (GetComponent<BoxCollider2D>().bounds.extents.x - 0.1f), transform.position.y), Vector2.down, distToGround + 0.1f, groundLayer);
+        RaycastHit2D groundCheck2 = Physics2D.Raycast(new Vector2(transform.position.x + (GetComponent<BoxCollider2D>().bounds.extents.x - 0.1f), transform.position.y), Vector2.down, distToGround + 0.1f, groundLayer);
         //Debug.Log(groundCheck.collider.gameObject.name);
 
-        if(groundCheck.collider != null)
+        if (groundCheck.collider != null || groundCheck2.collider != null)
         {
             grounded = true;
         }
--- Assets/Scripts/DexterMovement.cs
+++ Assets/Scripts/DexterMovement.cs
@@ -1,18 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class DexterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public KeyCode jump = KeyCode.W;
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode moveLeft = KeyCode.A;
+    public float horizSpeed = 3.0f;
+    public float jumpPower = 6.0f;
+    public bool ragdolling;
+    float distToGround;
+    //public float boundY = 2.25f; PUT THIS BACK IN TO LIMIT FROM FALLING OFF-SCREEN
+    private Rigidbody2D rb2d;
+    // Use this for initialization
     void Start()
     {
-        
+        ragdolling = false;
+        rb2d = GetComponent<Rigidbody2D>();
+        distToGround = GetComponent<Collider2D>().bounds.extents.y + 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 vel = rb2d.velocity;//velocity is set to current velocity
+        if (!ragdolling)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            if (Input.GetKey(jump))
+            {
+                //if touching ground:
+                if (GetComponent<CheckGrounded>().grounded)
+                {
+                    vel.y = jumpPower; //placeholder jump height value
+                }
+            }
+            if (Input.GetKey(moveRight))
+            {
+                //RaycastHit2D wallCheck = Physics2D.Raycast(new Vector2(transform.position.x - (GetComponent<BoxCollider2D>().bounds.extents.x - 0.1f), transform.position.y), Vector2.down, distToGround + 0.1f, groundLayer);
+                if (vel.x <= horizSpeed)
+                {
+                    vel.x = vel.x + 1;
+                }
+            }
+            else if (Input.GetKey(moveLeft))
+            {
+                if (vel.x >= -horizSpeed)
+                {
+                    vel.x = vel.x - 1;
+                }
+            }
+            else
+            {
+                vel.x /= 2;
+            }
+
+            if (GetComponent<CheckGrounded>().grounded && vel.y < 0)
+            {
+                vel.y = 0;
+
+                rb2d.velocity = vel;
+            }
+            else
+            {
+                vel.y -= 0.01f;
+            }
+
+            rb2d.velocity = vel;
+            Debug.Log(vel);
+        }
         
+
+        /*var pos = transform.position;
+		if (pos.y > boundY)
+		{
+			pos.y = boundY;
+		}
+		else if (pos.y < -boundY)
+		{
+			pos.y = -boundY;
+		}
+		transform.position = pos;*/
+
     }
 }
