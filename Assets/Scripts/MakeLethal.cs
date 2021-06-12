6add64041b1cf76f815501f3d05bc14cfd1626e8
NotQuiteCosmic <emersonodomj@gmail.com>
2021-06-12 19:09:07 -0400

    shog

---
+++ Assets/Scripts/MakeLethal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeLethal : MonoBehaviour
+{
+    private PlayerController c;
+
+
+    void Awake()
+    {
+        c = FindObjectOfType<PlayerController>();
+        Debug.Log(c.gameObject.name);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject == c.gameObject)
+        {
+            c.kill();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool isDead = false;
+
+
+    public void kill()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex);
+    }
+}
