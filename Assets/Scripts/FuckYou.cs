de9a8e5f94e0d03f0511b4b71aff52464ab5cbdb
deviousbat <alecj66@gmail.com>
2021-06-12 22:32:20 -0400

    Barrel?

--- Assets/Scripts/FuckYou.cs
+++ Assets/Scripts/FuckYou.cs
@@ -2,34 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class splodyBarrel : MonoBehaviour
+public class oceanAudio : MonoBehaviour
 {
     public float area;
     public float force;
     public LayerMask LayerToHit;
-
-    public GameObject ExplodeFX;
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    // Start is called before the first frame update
+    void Start()
     {
-        if (collision.gameObject.GetComponent<MakeLethal>() != null)
-        {
-            explode();
-        }
+        
     }
 
-    private void explode()
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void explode()
     {
         Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, area, LayerToHit);
 
-        foreach (Collider2D obj in objects)
+        foreach(Collider2D obj in objects)
         {
             Vector2 direction = obj.transform.position - transform.position;
 
             obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
         }
-
-        GameObject ExplodeFXIns = Instantiate(ExplodeFX, transform.position, Quaternion.identity);
-        Destroy(ExplodeFXIns, 10);
     }
 }
