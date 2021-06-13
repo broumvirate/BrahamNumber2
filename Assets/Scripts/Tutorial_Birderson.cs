14a55631cd077298a24c7a7e2a945299ad3b4ddf
HenBagle <ben.hagle@gmail.com>
2021-06-13 10:38:49 -0400

    Initila tutorial stuff

---
+++ Assets/Scripts/Tutorial_Birderson.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Birderson : MonoBehaviour
+{
+    public BoxCollider2D boxEnter;
+    public GameObject birdWithChain;
+    private CameraControls cam;
+
+    private bool hasBeenTriggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        birdWithChain.SetActive(false);
+        cam = FindObjectOfType<CameraControls>();
+        cam.birdFucker = cam.dexter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.GetComponent<BoxCollider2D>() == boxEnter && !hasBeenTriggered)
+        {
+            hasBeenTriggered = true;
+            StartCoroutine(EnableBird());
+        }
+    }
+
+    public IEnumerator EnableBird()
+    {
+        birdWithChain.SetActive(true);
+        cam.birdFucker = birdWithChain.GetComponentInChildren<BirdMovement>().gameObject;
+        yield return null;
+    }
+
+}
