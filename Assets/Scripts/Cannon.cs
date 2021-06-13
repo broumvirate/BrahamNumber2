aa093084cb70016b9ca19367e7a1fa13e8358389
HenBagle <ben.hagle@gmail.com>
2021-06-13 02:32:41 -0400

    Make cannon more like braham canno

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -7,7 +7,7 @@ public class Cannon : MonoBehaviour
     // Start is called before the first frame update
     public float frequency = 2f;
     public float bulletTravelDistance = 10f;
-    public float force = 1f;
+    public float force = 5f;
     public GameObject BulletPrefab;
     private Transform loc;
     void Start()
@@ -24,10 +24,13 @@ public class Cannon : MonoBehaviour
 
     void Fire()
     {
-        var bulletObj = Instantiate(BulletPrefab, loc.position, Quaternion.Euler(new Vector3(0,0,90)));
+        var angle = loc.localEulerAngles.z + 90;
+        var bulletObj = Instantiate(BulletPrefab, loc.position, Quaternion.Euler(new Vector3(0,0,angle)));
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), bulletObj.GetComponent<Collider2D>());
         var bulletComponent = bulletObj.GetComponent<CannonBullet>();
         bulletComponent.distance = bulletTravelDistance;
-        bulletObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(-force, 0));
+        bulletObj.GetComponent<Rigidbody2D>()
+            .AddForce(bulletObj.transform.TransformDirection(new Vector2(0, 1)).normalized * force,
+                ForceMode2D.Impulse);
     }
 }
