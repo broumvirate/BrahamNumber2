6392ea52f2b2d0e4744daef210127df82af7cb08
NotQuiteCosmic <emersonodomj@gmail.com>
2021-06-13 09:41:01 -0400

    Woah

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -9,6 +9,8 @@ public class Cannon : MonoBehaviour
     public float bulletTravelDistance = 10f;
     public float force = 5f;
     public GameObject BulletPrefab;
+    public GameObject ShotTarg;
+    public GameObject Wheel;
     private Transform loc;
     void Start()
     {
@@ -16,17 +18,11 @@ public class Cannon : MonoBehaviour
         InvokeRepeating("Fire", frequency, frequency);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void Fire()
     {
         // Make the bullet
-        var angle = loc.localEulerAngles.z + 90;
-        var bulletObj = Instantiate(BulletPrefab, loc.position, Quaternion.Euler(new Vector3(0,0,angle)));
+        var angle = loc.eulerAngles.z + 90;
+        var bulletObj = Instantiate(BulletPrefab, ShotTarg.transform.position, Quaternion.Euler(new Vector3(0,0,angle)));
         
         // Don't let it touch me
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), bulletObj.GetComponent<Collider2D>());
